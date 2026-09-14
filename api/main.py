189315f4d9ab1f8f73d0a576ddabe74edd245dee
Aun60 <aunnaeem52@gmail.com>
2026-09-14 16:52:17 +0500

    update api

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="Fiqh Multi-Agent RAG API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "fiqh-agent-system.vercel.app",
+        "https://fiqh-agent-system.vercel.app",
         "http://localhost:5500",  # keep for local testing
     ],
     allow_methods=["*"],
